c1eb0376eb095d7b923ece5348a44338e7fba942
Pojoke <pavlykdmutro@gmail.com>
2025-01-23 19:13:19 +0200

    dz

--- db.php
+++ db.php
@@ -9,7 +9,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 } 
-$sql = "SELECT * FROM Tovar ORDER BY `Назва`";
+$sql = "SELECT * FROM Tovar ORDER BY nazva";
 $result = $conn->query($sql); 
 
 // if ($result->num_rows > 0) {
