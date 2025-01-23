204f43dcd1bcd7462a03c9d668f1ae90a7fbab17
Pojoke <pavlykdmutro@gmail.com>
2025-01-23 19:16:16 +0200

    dz

---
+++ bet.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dz";
+// Створення з'єднання 
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Перевірка з'єднання  
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT id,nazva,prodaz FROM Tovar WHERE prodaz>50 AND cena BETWEEN 30 AND 100";
+$result = $conn->query($sql); 
+
+// if ($result->num_rows > 0) {
+//      // output data of each row
+//      while ($row = $result->fetch_assoc()) {
+//          echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . " ";
+//      }
+//  } else {
+//      echo "0 results";
+//  } 
+$conn->close();
