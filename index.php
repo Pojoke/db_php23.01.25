c3ad617d0a11706941a0b1563e95d16142a36b88
Pojoke <pavlykdmutro@gmail.com>
2025-01-23 18:07:34 +0200

    dz

--- index.php
+++ index.php
@@ -16,6 +16,32 @@ nazva VARCHAR(30) NOT NULL,
 cena INT ,
 prodaz INT
 )";
+
+
+
+
+
+$sql = "INSERT INTO Tovar (nazva, cena, prodaz) 
+ VALUES ('Хліб', '25 ', '200 ');";
+$sql .= "INSERT INTO Tovar (nazva, cena, prodaz) 
+ VALUES ('Молоко', '30 ', '150 ');";
+$sql .= "INSERT INTO Tovar (nazva, cena, prodaz)  
+ VALUES ('Яблуко', '10 ', '100 ');";
+ $sql .= "INSERT INTO Tovar (nazva, cena, prodaz)  
+ VALUES ('Банани', '15 ', '120 ');";
+ $sql .= "INSERT INTO Tovar (nazva, cena, prodaz)  
+ VALUES ('Кава', '150 ', '300 ');";
+if ($conn->multi_query($sql) === TRUE) {
+    echo "New records created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+
+
+
+
 if (mysqli_query($conn, $sql)) {
     echo "Table MyGuests created successfully";
 } else {
